09b21506c64a571c450d0eb68d3a295a71ca0783
Domenico Moreschini <tech593@yoox.com>
2017-12-18 12:39:18 +0100

    first commit

---
+++ src/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace tesetBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
