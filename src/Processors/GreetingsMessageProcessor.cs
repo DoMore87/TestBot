0ad4387961108a35f2342894607cbecbbada9c15
Domenico <domenico.moreschini@gmail.com>
2017-12-21 17:43:21 +0100

    Add VS2017 sln file

--- src/Processors/GreetingsMessageProcessor.cs
+++ src/Processors/GreetingsMessageProcessor.cs
@@ -1,5 +1,3 @@
-using System.Text.RegularExpressions;
-
 namespace TestBot.Processors
 {
     public class GreetingsMessageProcessor : RegexMessageProcessor
--- src/Processors/HowAreYouMessageProcessor.cs
+++ src/Processors/HowAreYouMessageProcessor.cs
@@ -2,17 +2,17 @@ namespace TestBot.Processors
 {
     public class HowAreYouMessageProcessor : RegexMessageProcessor
     {
-        public HowAreYouMessageProcessor(IMessageProcessor successor) : base(successor)
-        {
-        }
-
         protected override string RegexPattern => @"^Come +(va|butta|stai)\??";
 
-        protected override string[] Responses => new [] { "Alla grande!",
+        protected override string[] Responses => new[] { "Alla grande!",
         "Non c'è male, grazie",
         "Sono un bot semplice, mi basta poco per essere felice",
         "Bene, bene",
         "Non potrbbe andare meglio!",
         "Come al solito, senza infamia e senza lode" };
+
+        public HowAreYouMessageProcessor(IMessageProcessor successor) : base(successor)
+        {
+        }
     }
 }
